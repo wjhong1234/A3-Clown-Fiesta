c45412fc52f093cbdae5f823e243a037eafb784d
wheezingGekko <farrah.s.urmeneta@hotmail.ca>
2016-06-05 17:46:35 -0600

    Updated map's tiles

--- map.s
+++ map.s
@@ -1,11 +1,5 @@
-.section .init
-.global map
-
-
 /*
 Instance of game environment
-	n x m grid
-	n & m >= 20 tiles	
 	specifies fuel of player
 	specifies number of lives left
 	specifies what each cell of grid is (road/side tile)
@@ -14,22 +8,41 @@ Instance of game environment
 
 */
 .section .text
-map:
+	// adjust the latter number where necessary
+	.equ	END, 24 * 8	// end of the map
+	
+	MAP_Y .req r4
 	
 
+initializeMap:
+	push	{r4-r10, lr}
+
+	//bl	drawMap
+	
+
+	pop	{r4-r10, lr}
+	bx	lr
+
+mapState:
+	
+
+updateMap:
+	
 
 
 
 .section .data
 	// total 32 * 24 tiles
+/*
 leftSide:
-	.skip 216 * 4	// 24 * 9 tiles
+	.skip 264 * 4	// 24 * 11 tiles
 	.end
 rightSide:
 	.skip 120 * 4	// 24 * 5 tiles 
 	.end
+*/
 road:
-	.skip 432 * 4	// 24 * 18 tiles
+	.skip 384 * 4	// 24 * 16 tiles
 	.end		
 
 
