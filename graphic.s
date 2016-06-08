86fad2c59e66648c05456d8c542b955e46177026
susant-pant <susantpant@hotmail.com>
2016-06-07 18:38:02 -0600

    Update graphic.s

--- graphic.s
+++ graphic.s
@@ -78,64 +78,6 @@ DrawPixel:
 	pop		{r4}
 	bx		lr
 
-/* Draw a character to (x,y)
-character: r0
-colour: r1
-x-coord: r2
-y-coord: r3
- */
-.globl DrawChar
-DrawChar:
-	push	{r4-r10, lr}
-
-	chAdr	.req	r4
-	px	.req	r5
-	py	.req	r6
-	row	.req	r7
-	mask	.req	r8
-
-	mov		py,	r3		// init the Y coordinate (pixel coordinate)
-	mov		r9,	r2		// save X coordinate at register 9
-	mov		r10, 	r1		// save colour at register 10
-	ldr		chAdr,	=font		// load the address of the font map
-	add		chAdr,	r0, lsl #4	// char address = font base + (char * 16)
-
-charLoop$:
-	mov		px,	r9			// init the X coordinate
-
-	mov		mask,	#0x01		// set the bitmask to 1 in the LSB
-	
-	ldrb	row,	[chAdr], #1	// load the row byte, post increment chAdr
-
-rowLoop$:
-	tst		row,	mask		// test row byte against the bitmask
-	beq		noPixel$
-
-	mov		r0,		px
-	mov		r1,		py
-	mov		r2,		r10		// red
-	bl		DrawPixel			// draw red pixel at (px, py)
-
-noPixel$:
-	add		px,		#1			// increment x coordinate by 1
-	lsl		mask,	#1			// shift bitmask left by 1
-
-	tst		mask,	#0x100		// test if the bitmask has shifted 8 times (test 9th bit)
-	beq		rowLoop$
-
-	add		py,		#1			// increment y coordinate by 1
-
-	tst		chAdr,	#0xF
-	bne		charLoop$			// loop back to charLoop$, unless address evenly divisibly by 16 (ie: at the next char)
-
-	.unreq	chAdr
-	.unreq	px
-	.unreq	py
-	.unreq	row
-	.unreq	mask
-
-	pop		{r4-r10, pc}
-
 .globl	clearScreen
 
 clearScreen:
